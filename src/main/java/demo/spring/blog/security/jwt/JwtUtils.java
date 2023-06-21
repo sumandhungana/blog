b37db05aae0ee7d66524d43867acb6eb6a390612
sumandhungana <suman.dhungana@citytech.global>
2023-06-21 21:35:17 +0545

    Change jwt security info

--- src/main/java/demo/spring/blog/security/jwt/JwtUtils.java
+++ src/main/java/demo/spring/blog/security/jwt/JwtUtils.java
@@ -17,10 +17,10 @@ import java.util.Date;
 public class JwtUtils {
   private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-  @Value("${bezkoder.app.jwtSecret}")
+  @Value("${security.app.jwtSecret}")
   private String jwtSecret;
 
-  @Value("${bezkoder.app.jwtExpirationMs}")
+  @Value("${security.app.jwtExpirationMs}")
   private int jwtExpirationMs;
 
   public String generateJwtToken(Authentication authentication) {
